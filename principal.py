e359aa833287a38664717bf9f253d30f6d4fa20a
JairoDH <jairodominguez3594@gmail.com>
2023-05-09 10:33:02 +0200

    ejercicio1
    Realización del Programa 1 en Python

--- principal.py
+++ principal.py
@@ -14,7 +14,7 @@ print("	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 TAG = input("Ingresa el TAG del jugador: ")
 
 url_base = f"https://api.brawlstars.com/v1/players/%23{TAG}"
-KEY = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk3ZjBiNTBhLWEyMDQtNGU2My05MTRjLTdhNmI4YjBjZDJjNCIsImlhdCI6MTY4MzYxMzc4Miwic3ViIjoiZGV2ZWxvcGVyL2Y2MWE1ZDlhLTA1YjEtZGE4My01Nzc2LTkyM2JmYTVjZjU5YyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTU4Ljk5LjEuMjQiXSwidHlwZSI6ImNsaWVudCJ9XX0.D0LHA0oEPDBs0QQG7LKntnc1gTYCyQ-xFIE3WXKhNNCc2elxaWovyQotFj2qsKyDdDD5Bz3NyHryeK0IlVl5jw'
+KEY = 'API-KEY'
 headers = {'Authorization': KEY}
 response = requests.get(url_base, headers=headers)
 
